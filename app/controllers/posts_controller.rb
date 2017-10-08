d3fe95f12ccdd105d598e701a48ca3bf328ccc54
Sergiiss <Dracula23@mail.ru>
2017-10-08 22:59:22 +0300

    Add actions

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -18,10 +18,36 @@ class PostsController < ApplicationController
   end
 
   def show
-    @post = Post.find(params[:id])
+    set_post
+  end
+
+  def edit
+    set_post
+  end
+
+  def update
+    set_post
+
+    if @post.update_attributes(post_params)
+      redirect_to @post
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    set_post
+
+    @post.destroy
+
+    redirect_to root_path
   end
 
   private
+
+  def set_post
+    @post = Post.find(params[:id])
+  end
 
   def post_params
     params.require(:post).permit(:title, :body)
