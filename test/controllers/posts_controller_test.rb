bef0a4ac2d714c8415f00a701f56503660456014
Sergiiss <Dracula23@mail.ru>
2017-12-10 15:21:13 +0300

    Start writing tests for the controller

--- test/controllers/posts_controller_test.rb
+++ test/controllers/posts_controller_test.rb
@@ -1,7 +1,33 @@
 require 'test_helper'
 
 class PostsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should get index" do
+    get root_path
+
+    assert_response :success
+
+    assert_select "h1", "List of posts"
+  end
+
+  test "should get new" do
+    get new_post_path
+
+    assert_response :success
+
+    assert_select "form input", 3
+  end
+
+  test "should get show" do
+    post = Post.first
+
+    get post_path(post)
+
+    if post.nil?
+      assert_response :missing
+    else
+      assert_response :success
+
+      assert_select "div", 7
+    end
+  end
 end
